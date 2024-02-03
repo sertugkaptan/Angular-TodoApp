3831eb540e044c97ecc813891d59634862f386bd
sertukaptan <sertuhan00@gmail.com>
2024-02-03 15:39:52 +0100

    Last push Add,Remove,Done functionalities Working

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,30 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
 import { Todo } from '../../models/Todo';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
-export class TodosComponent implements OnInit{
-  todos:Todo[];
-  
-  constructor(){
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  inputTodo: string | undefined;
+
+  constructor() {
     this.todos = [];
   }
-
   ngOnInit(): void {
-    this.todos= [{
-      id:0,
+    this.todos = [{
       content: 'First Todo',
-      completed:false
-    },{
-      id:0,
+      completed: false
+    }, {
       content: 'Second Todo',
-      completed:true
+      completed: true
     }
-  ];
+    ];
   }
 
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) {
+        v.completed = !v.completed;
+      }
+      return v;
+    })
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i != id);
+  }
+
+  addTodo(){
+    let todo={content:this.inputTodo,completed:false};
+    this.todos.push(todo);
+
+    this.inputTodo = "";
+  }
 }
--- src/app/models/Todo.ts
+++ src/app/models/Todo.ts
@@ -1,5 +1,5 @@
 export class Todo{
-    id:Number|undefined;
     content:string | undefined;
     completed:boolean | undefined;
+
 }
